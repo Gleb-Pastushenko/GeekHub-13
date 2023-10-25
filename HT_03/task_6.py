7c1b20946d82d7680381643bdecf845c0a956dbf
Гліб Пастушенко <gleb.pastushenko.it@gmail.com>
2023-10-25 12:30:16 +0300

    All tasks reworked

--- HT_03/task_6.py
+++ HT_03/task_6.py
@@ -1,21 +1,29 @@
 # Write a script to get the maximum and minimum value in a dictionary
 
 from text_decoration import doubleLine, singleLine, emptyLine
-from tasks_data import task_6_data
+
+NUMS = (int, float, bool)
+ITERS = (list, tuple, set, frozenset, dict)
 
 
-def isNumberValue(val):
-    try:
-        float(val)
-        return True
-    except:
-        pass
+def getVals(type, vals):
+    return list(filter(lambda val: isinstance(val, type), vals))
+
+
+def showMinMax(type_name, values):
+    if isinstance(values[0], ITERS):
+        print(
+            f'{type_name} values:\n    min: {min(values, key=len)}, max: {max(values, key=len)}')
+    else:
+        print(f'{type_name} values:\n    min: {min(values)}, max: {max(values)}')
 
 
 # TASK DATA
-task_6_data = {1: '23', 'two': [1, 3, 3], 3: 1.5, 'four': '4.4'}
+task_6_data = {1: '23', 'two': [1, 3, 3], 3: 1.5, 'four': '4.4', "dic": {
+    'a': 'a', 'b': 'b', 'c': 'c', 'd': 'c'}, 'empty': ()}
 
 if __name__ == "__main__":
+    # Intro and data prompting
     doubleLine()
 
     print('Here is the initial dict:')
@@ -24,12 +32,21 @@ if __name__ == "__main__":
 
     singleLine()
 
-    number_values = filter(isNumberValue, task_6_data.values())
-    values = [float(value) if float(value) % 1 != 0 else int(value)
-              for value in number_values]
+    # The task itself
+    values = task_6_data.values()
 
-    print("The min value is:  " + str(min(values)))
-    emptyLine()
-    print("The max value is:  " + str(max(values)))
+    num_vals = getVals(NUMS, values)
+    str_vals = getVals(str, values)
+    iter_vals = getVals(ITERS, values)
+
+    # Printing out the result
+    if num_vals:
+        showMinMax('Number', num_vals)
+
+    if str_vals:
+        showMinMax('String', str_vals)
+
+    if iter_vals:
+        showMinMax('Iterable', iter_vals)
 
     doubleLine()
