b3f1f41c2afe22dd977af13f216919e271734bbc
Гліб Пастушенко <gleb.pastushenko.it@gmail.com>
2024-01-09 15:30:18 +0200

    Model field names fixed (changed from camelCase to snake_case)

--- HT_18/goods_parsing/parse/models.py
+++ HT_18/goods_parsing/parse/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Product(models.Model):
     id = models.CharField(max_length=10, primary_key=True)
     name = models.CharField(max_length=100)
-    brandName = models.CharField(max_length=100)
-    regularPrice = models.DecimalField(max_digits=10, decimal_places=2)
+    brand_name = models.CharField(max_length=100)
+    regular_price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     link = models.CharField(max_length=100)
 
