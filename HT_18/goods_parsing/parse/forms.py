d631bd9feb442e374c6ffc0ba0ea291f727ecad9
Гліб Пастушенко <gleb.pastushenko.it@gmail.com>
2024-01-10 03:10:54 +0200

    forms.py: CharField max_length set to 100

--- HT_18/goods_parsing/parse/forms.py
+++ HT_18/goods_parsing/parse/forms.py
@@ -3,5 +3,6 @@ from django import forms
 
 class EnterIdsForm(forms.Form):
     id_list = forms.CharField(
-        label="Enter products IDs, separated by coma"
+        label="Enter products IDs, separated by coma",
+        max_length=100,
     )
